de79d074dee9d581af1794f3ca7a10af55623a8c
Mahyar Sameti <Mahyar@sealand110.com>
2023-05-03 15:20:01 +0330

    update

--- src/main/java/org/example/Customer.java
+++ src/main/java/org/example/Customer.java
@@ -1,6 +1,25 @@
 package org.example;
 
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
 public class Customer {
+
+    public void Csv() throws Exception{
+        try{
+            String path = "D:\\Projects\\Customer.csv";
+            Reader reader = Files.newBufferedReader(Paths.get(path));
+
+        }catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+
+
+
+    }
     private int RecordNumber;
     private int CustomerID;
     private String CustomerName;
